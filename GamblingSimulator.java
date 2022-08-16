bcac666811adf34a4282f5102da9ce7f07bddf84
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-16 21:30:46 +0530

    Added: bet 1 every game

---
+++ GamblingSimulator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.workshop;
+
+public class GamblingSimulator {
+        static int stake_cash= 100;
+        static int bet=1;
+        public static void main(String[] args) {
+            System.out.println("Cash of stake start game of everyday is :"+stake_cash);
+            System.out.println("cash to bet after every game " + bet);
+}}
